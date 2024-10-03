168e87273403239ab6816ed05287c7897b3474f7
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2024-10-03 20:42:38 +0100

    correct relative import problem

--- data_scripts/thegraph/thegraph.py
+++ data_scripts/thegraph/thegraph.py
@@ -20,7 +20,6 @@ headers = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
 }
 
 data = {
