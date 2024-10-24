32fb6f167b362d2b0d9c761fc69b22463730b327
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2024-10-24 21:52:32 +0100

    update

--- my-token/MyToken.sol
+++ my-token/MyToken.sol
@@ -16,35 +16,24 @@ contract MyToken {
   );
 
   // TODO: create mapping for balances
-  mapping(address => uint256) balances;
 
   // TODO: create mapping for allowances
-  mapping(address => mapping(address => uint256)) allowances;
 
   constructor() {
     // TODO: set sender's balance to total supply
-    balances[msg.sender] = supply;
   }
 
   function totalSupply() public pure returns (uint256) {
     // TODO: return total supply
-    return supply;
   }
 
   function balanceOf(address _owner) public view returns (uint256) {
     // TODO: return the balance of _owner
-    return balances[_owner];
   }
 
   function transfer(address _to, uint256 _value) public returns (bool) {
     // TODO: transfer `_value` tokens from sender to `_to`
     // NOTE: sender needs to have enough tokens
-    require(balances[msg.sender] >= _value, "Not enough balance.");
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    emit Transfer(msg.sender, _to, _value);
-
-    return true;
   }
 
   function transferFrom(
@@ -54,24 +43,11 @@ contract MyToken {
   ) public returns (bool) {
     // TODO: transfer `_value` tokens from `_from` to `_to`
     // NOTE: `_from` needs to have enough tokens and to have allowed sender to spend on his behalf
-    require(balances[_from] >= _value, "Not enough balance.");
-    require(allowances[_from][msg.sender] >= _value, "Not enough allowance.");
-    allowances[_from][msg.sender] -= _value;
-    balances[_from] -= _value;
-    balances[_to] += _value;
-    emit Transfer(_from, _to, _value);
-
-    return true;
   }
 
   function approve(address _spender, uint256 _value) public returns (bool) {
     // TODO: allow `_spender` to spend `_value` on sender's behalf
     // NOTE: if an allowance already exists, it should be overwritten
-    allowances[msg.sender][_spender] = _value;
-
-    emit Approval(msg.sender, _spender, _value);
-
-    return true;
   }
 
   function allowance(address _owner, address _spender)
@@ -80,6 +56,5 @@ contract MyToken {
     returns (uint256 remaining)
   {
     // TODO: return how much `_spender` is allowed to spend on behalf of `_owner`
-    return allowances[_owner][_spender]; 
   }
 }
